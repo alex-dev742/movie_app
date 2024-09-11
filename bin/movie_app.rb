f90c555dad388d45ae9593098a701513824b1123
Alex <alex@pchoice.co.za>
2024-09-11 10:55:50 +0200

    added user input

--- bin/movie_app.rb
+++ bin/movie_app.rb
@@ -6,7 +6,19 @@ playlist.add_movie("22 Jump Street")
 playlist.add_movie("Game of Thrones")
 playlist.add_movie("The 100")
 
+loop do
+  print "How many plays (quit to exit): "
+  answer = gets.chomp
 
+  case answer
+    when /^\d+$/
+      playlist.play
+    when "quit", "exit"
+      puts "\nQuiting Program!"
+      break
+    else
+      "Please select how many plays you want"
+  end
+end
 
-playlist.play
 playlist.list_movies
